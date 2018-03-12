c13e1302edda5489c71af1395b75a453f9d27042
Marcos Carvalho <marcosabcarvalho@yahoo.com>
2018-03-12 17:54:20 -0300

    Add rectangle module

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var rect = {
-  perimeter: (x,y) => (2*(x+y)),
-  area: (x,y) => (x*y)
-};
+var rect = require('./rectangle');
+
+/*
+    {
+        perimeter: (x, y) => (2 * (x + y)),
+        area:  (x, y) => (x * y)
+    };
+*/
+
 
 function solveRect(l,b) {
     console.log("=================================================================================");
